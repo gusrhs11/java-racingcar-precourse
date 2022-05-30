7ef39237ec67f0d1c59dd75ecd720b1f1cb947b1
HyunGon <gusrhs11@gmail.com>
2022-05-31 00:56:56 +0900

    feat(Car) : implement move, print, getPosition method
    Added new method to Car class:
    - move : random move
    - print : print current position
    - getPostion : get current position

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -7,7 +7,24 @@ public class Car {
     public Car(String name) {
         this.name = name;
     }
-    int please = 0;
+    public void move(){
+        int randNum = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9);
+        if(randNum >= 4){
+            position++;
+            return;
+        }
+    }
+    public void print(){
+        System.out.print(name + " : ");
+        for(int i = 0; i < position; i++){
+            System.out.print('-');
+        }
+        System.out.println();
+    }
+
+    public int getPosition(){
+        return position;
+    }
 
     // 추가 기능 구현
 }
