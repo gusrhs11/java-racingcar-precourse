0a35e37aac88526b7bd4e23f3d238e3a890f10ec
HyunGon <gusrhs11@gmail.com>
2022-05-30 17:48:36 +0900

    sss

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,7 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("I want to know which is wrong ㅠㅠ");
+        System.out.println("I want to know which is wrong ㅠ");
         // TODO 구현 진행
     }
 }
