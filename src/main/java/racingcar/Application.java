4802cb3cc2275d3048a5ad8a9b9fa3761d128c06
HyunGon <gusrhs11@gmail.com>
2022-05-31 00:58:44 +0900

    feat(getName) : get name and thorw exception
    Added new function:
    - get names and split into string[] array

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,28 @@
 package racingcar;
 
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("I want to know which is wrong ");
         // TODO 구현 진행
-        System.out.println("start");
-        System.out.println("start");
+        String Input = camp.nextstep.edu.missionutils.Console.readLine();
+        String NumOfgame = camp.nextstep.edu.missionutils.Console.readLine();
+        String[] name = Input.split(",");
+        Car[] car = new Car[name.length];
+        try{
+            for(int i = 0; i < name.length; i++){
+                if(name[i].length() > 5){
+                    throw new IllegalArgumentException("[ERROR] 이름은 5글자 이내입니다.");
+                }
+            }
+        }catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        for(int i = 0; i < name.length; i++){
+            car[i] = new Car(name[i]);
+        }
 
     }
 }
