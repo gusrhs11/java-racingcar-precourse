bd729f03b6f8843e47c795729d48a2572b1efb3c
HyunGon <gusrhs11@gmail.com>
2022-05-30 18:28:12 +0900

    start

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,7 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("I want to know which is wrong ㅠ");
+        System.out.println("I want to know which is wrong ");
         // TODO 구현 진행
     }
 }
