ac189edc59e9c6973945052ed596a2ba5f3a02b3
HyunGon <gusrhs11@gmail.com>
2022-06-03 00:18:53 +0900

    feature : game manager
    Functions:
    - exception functions
    - game progress
    - game winner

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,22 +9,32 @@ public class Application {
         // TODO 구현 진행
 
         String[] name = nameExceptionRecursion();
-
+        int numOfGame = numberExceptionRecursion();
+        int maxLen = -1;
         Car[] car = new Car[name.length];
         for(int i = 0; i < name.length; i++){
             car[i] = new Car(name[i]);
         }
-        /*
-        for(iter = 0; iter < numOfGame.length(); iter++){
-            if((int)numOfGame.charAt(iter) <= 9 && (int)numOfGame.charAt(iter) >= 0){
-                continue;
+
+        for(int i = 0; i < numOfGame; i++){
+            for(int j = 0; j < name.length; j++){
+                car[j].move();
+                car[j].print();
             }
-            break;
+            System.out.println();
         }
-        if(iter == numOfGame.length()){
-            numOfGame_int = Integer.parseInt(numOfGame);
+
+        for(int i = 0; i < name.length; i++){
+            if(car[i].getPosition() > maxLen){
+                maxLen = car[i].getPosition();
+            }
         }
-        */
+        for(int i = 0; i < name.length; i++){
+            if(car[i].getPosition() == maxLen){
+                System.out.print(car[i].getName());
+            }
+        }
+
 
 
     }
@@ -44,15 +54,29 @@ public class Application {
         }
         return name;
     }
-}
 
-    private static String numberExceptionRecursion() {
+    private static int numberExceptionRecursion() {
         String numOfGame = camp.nextstep.edu.missionutils.Console.readLine();
-        int numOfGame_int;
-        try{
+        int numOfGame_int = 0;
+        try {
             numOfGame_int = Integer.parseInt(numOfGame);
-        }catch(IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 숫자를 입력하십시오");
             numberExceptionRecursion();
         }
+        return numOfGame_int;
+    }
 }
+
+
+        /*
+        for(iter = 0; iter < numOfGame.length(); iter++){
+            if((int)numOfGame.charAt(iter) <= 9 && (int)numOfGame.charAt(iter) >= 0){
+                continue;
+            }
+            break;
+        }
+        if(iter == numOfGame.length()){
+            numOfGame_int = Integer.parseInt(numOfGame);
+        }
+        */
